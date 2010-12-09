b14013543a899f60f73f37053c6efa72f5d9a970
sajib1223 <sajib1223@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-12-09 07:30:22 +0000

    Version 1.1 modified files
    git-svn-id: https://plugins.svn.wordpress.org/w4-post-list/trunk@320860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- w4-post-list.php
+++ w4-post-list.php
@@ -3,23 +3,22 @@
 Plugin Name: W4 post list
 Plugin URI: http://w4dev.com/w4-plugin/w4-post-list
 Description: List your wordpress posts as you like in post or page or in sidebar widgets. !!
-Version: 1.0
+Version: 1.1
 Author: Shazzad Hossain Khan
 Author URI: http://w4dev.com/
 */
 define( 'W4PL_DIR', plugin_dir_path(__FILE__)) ;
 define( 'W4PL_URL', plugin_dir_url(__FILE__)) ;
 define( 'W4PL_BASENAME', plugin_basename( __FILE__ )) ;
-define( 'W4PL_VERSION', '1.0' ) ;
+define( 'W4PL_VERSION', '1.1' ) ;
 define( 'W4PL_NAME', 'W4 post list' ) ;
 define( 'W4PL_SLUG', strtolower(str_replace(' ', '-', W4PL_NAME ))) ;
 
 //Load our script and css file
-class W4PL {
-	function W4PL(){
+class W4PL_CORE {
+	function W4PL_CORE(){
 		add_action( 'init', array(&$this, 'load_w4pl_scripts')) ;
 		add_action( 'admin_menu', array(&$this, 'admin_menu')) ;
-		add_action( 'widgets_init', array(&$this, 'load_w4pl_widget')) ;
 		add_action( "admin_init", array(&$this, 'add_post_meta')) ;
 		add_action( 'save_post', array(&$this, 'save_details'));
 		add_action( 'post_row_actions', array(&$this, 'post_row_actions'));
@@ -28,14 +27,8 @@ class W4PL {
 
 	//Load scripts
 	function load_w4pl_scripts(){
-		wp_enqueue_script( 'w4pl_js', W4PL_URL . '/w4-post-list.js', array( 'jquery' , 'jquery-ui-core' ), '',false ) ;
+		//wp_enqueue_script( 'w4pl_js', W4PL_URL . '/w4-post-list.js', array( 'jquery' , 'jquery-ui-core' ), '',false ) ;
 		wp_enqueue_style( 'w4pl_css', W4PL_URL . '/w4-post-list.css' ) ;
-	}
-
-	//Load widgets
-	function load_w4pl_widget(){
-		wp_register_sidebar_widget( 'widget_w4pl', 'W4 post list', array(&$this, 'widget_w4pl'), 'widget_w4pl');
-		wp_register_widget_control( 'widget_w4pl', 'W4 post list', array(&$this, 'w4pl_widget_control'), 'widget_w4pl');
 	}
 
 	function plugin_action_links( $links ){
@@ -57,154 +50,6 @@ class W4PL {
 		}
 
 		return array_merge($actions, $w4pl_post);
-	}
-
-	//Widget
-	function widget_w4pl( $args = array()){
-		extract( $args ) ;
-		$w4pl_options = get_option("widget_w4pl") ;
-	 
-		echo( $before_widget );
-		echo( $before_title );
-		echo( $w4pl_options['title'] );
-		echo( $after_title );
-		echo( "<div id=\"w4pl_sidebar\">" );
-		$this->widget_w4pl_sidebar() ;
-		echo( "</div>" );
-		echo( $after_widget );
-	}
-
-	//Widget
-	function widget_w4pl_sidebar(){
-		$w4pl_options = get_option("widget_w4pl") ;
-		$category_ids = $w4pl_options['w4pl_cat_ids'] ;
-		
-		if(empty($category_ids)){
-			echo "No category selected. Please select one to show its posts." ;
-			return ;
-		}
-		
-		echo "<ul class=\"w4pl_parent\">";
-		foreach($category_ids as $category_id){
-			global $post ;
-			$category = get_category($category_id) ;
-			$category_name = $category->name ;
-			$items = ($category->category_count > 0) ? "$category->category_count items" : "no items" ;
-			$items = " <span class=\"item_count\">$items</span>" ;
-
-			echo "<li class=\"w4pl_list\">" ;
-			echo "<h3 class=\"w4pl_cat_title\"><a href=\"".get_category_link($category_id)."\" title=\"View $category_name\">$category_name &raquo;</a>$items</h3>" ;
-			
-			if( $w4pl_options['w4pl_show_post'] == '1' ){
-			//query posts
-			query_posts(array('cat' => $category->cat_ID, 'showposts' => '-1', 'posts_per_page' => '-1', 'meta_key' => '_w4pl_post', 'meta_value' => '1' )) ;
-			//Checking post
-			if(have_posts()){
-				echo "<ul class=\"w4pl_sub\">" ;
-				while(have_posts()){
-					the_post() ;
-					$time = get_the_time( 'F jS, Y' ) ;
-					$post_title = "<a href=\"".get_permalink()."\" title=\"View ".get_the_title()."\">&raquo; ".get_the_title()."</a>" ;
-					echo "<li class=\"w4pl_post_list\">" ;
-					echo "<div class=\"w4pl_post_title\">$post_title - <small>$time</small></div>" ;
-
-					//Excerpt
-					if( $w4pl_options['w4pl_post_content'] == '1' ){
-						add_filter( 'excerpt_length', array(&$this, 'w4pl_excerpt_length'));
-						echo "<div class=\"w4pl_post_content\">".get_the_excerpt()."</div>" ;
-					}
-					
-					//Post content
-					if( $w4pl_options['w4pl_post_content'] == '2' ){
-						echo "<div class=\"w4pl_post_content\">".get_the_content()."</div>" ;
-					}
-
-					echo "</li>" ;
-				}				
-				echo "</ul>" ;
-			} //End have_posts()
-			} //Show post
-			echo "</li>" ;
-		}
-		wp_reset_query() ;
-		remove_filter( 'excerpt_length', array(&$this, 'w4pl_excerpt_length'));
-		echo "</ul>" ;
-	}
-	
-	
-	//Widget control
-	function w4pl_widget_control(){
-		$w4pl_default_options = array(
-			'title' => 'Post list',
-			'width' => '200',
-			'height' => '300',
-			'w4pl_excerpt_lenth' => '10',
-			'w4pl_cat_ids' => array(),
-			'w4pl_show_post' => '1',
-			'w4pl_post_content' => false
-		);
-		//update_option( "widget_w4pl", $w4pl_options ) ;
-
-		if( isset($_POST['w4pl_options'])){
-			//print_r($_POST['post_category']) ;
-			if( is_array($_POST['post_category'])){
-				$w4pl_options['w4pl_cat_ids'] = $_POST['post_category'] ;
-			}
-			
-			if(isset($_POST['w4pl_widget_title'])){
-				$w4pl_options['title'] = $_POST['w4pl_widget_title'] ;
-			}
-			
-			if(isset($_POST['w4pl_excerpt_lenth'])){
-				$w4pl_options['w4pl_excerpt_lenth'] = $_POST['w4pl_excerpt_lenth'] ;
-			}
-			
-			if(isset($_POST['w4pl_post_content'])){
-				$w4pl_options['w4pl_post_content'] = $_POST['w4pl_post_content'] ;
-			}
-			
-			if(isset($_POST['w4pl_show_post'])){
-				$w4pl_options['w4pl_show_post'] = $_POST['w4pl_show_post'] ;
-			}
-			
-			$w4pl_options = wp_parse_args( $w4pl_options, $w4pl_default_options ) ;
-			update_option( "widget_w4pl", $w4pl_options ) ;
-			//print_r($w4pl_options) ;
-		}
-
-		$get_w4pl_options = get_option("widget_w4pl") ;
-		?>
-		<div id="w4pl_widget_admin">
-			<input type="hidden" name="w4pl_options" id="w4pl_options" value="1" />
-			<p><label for="w4pl_widget_title">List Title:</label>
-				<input type="text" value="<?php echo( $get_w4pl_options['title']) ; ?>" name="w4pl_widget_title" id="w4pl_widget_title" class="widefat"/>
-            </p>
-            
-            <p>Show post list ?<br />
-				<label><input type="radio" name="w4pl_show_post" id="w4pl_show_post1" <?php if($get_w4pl_options['w4pl_show_post'] == '1' ) echo 'checked="checked"' ;?> value="1" /> Yes</label><br />
-                <label><input type="radio" name="w4pl_show_post" id="w4pl_show_post0" <?php if($get_w4pl_options['w4pl_show_post'] == '0' ) echo 'checked="checked"' ;?> value="0" /> No</label><br />
-            </p>
-            
-            <p>Show post content ?<br />
-				<label><input type="radio" name="w4pl_post_content" id="w4pl_post_content" <?php if($get_w4pl_options['w4pl_post_content'] == '0' ) echo 'checked="checked"' ;?> value="0" /> No</label><br />
-                <label><input type="radio" name="w4pl_post_content" id="w4pl_post_content" <?php if($get_w4pl_options['w4pl_post_content'] == '1' ) echo 'checked="checked"' ;?> value="1" />Show only excerpt</label><br />
-                <label><input type="radio" name="w4pl_post_content" id="w4pl_post_content" <?php if($get_w4pl_options['w4pl_post_content'] == '2' ) echo 'checked="checked"' ;?> value="2" />Show full content !!</label><br />
-            </p>
-            
-            <p><label for="w4pl_excerpt_lenth">Excerpt lenth for posts/pages:</label>
-				<input type="text" value="<?php echo( $get_w4pl_options['w4pl_excerpt_lenth']) ; ?>" name="w4pl_excerpt_lenth" id="w4pl_excerpt_lenth" class="widefat"/>
-            </p>
-			<p>
-            	Select category:<br />
-				<?php wp_terms_checklist( null, array( 'taxonomy' => 'category', 'selected_cats' => $get_w4pl_options['w4pl_cat_ids'] )) ; ?>
-		</div>
-	
-<?php
-	}
-	function w4pl_excerpt_length( $length ){
-		$get_w4pl_options = get_option("widget_w4pl") ;
-		$lenth = $get_w4pl_options['w4pl_excerpt_lenth'] ;
-		return $lenth ;
 	}
 
 	function add_post_meta(){
@@ -271,7 +116,189 @@ class W4PL {
 <?php
 	}
 }
+//Define widget======================
+class W4PL_Widget extends WP_Widget {
 
+	function W4PL_Widget() {
+		$widget_ops = array(
+					'classname' => 'w4_post_list',
+					'description' => __( "List selected posts from selected category...")
+				);
+		$control_ops = array('width' => 200, 'height' => 400);
+		$this->WP_Widget('w4_post_list', __('W4 post list'), $widget_ops,$control_ops );
+		$this->alt_option_name = 'w4_post_list';
+	}
+
+	function widget($args, $instance){
+		extract($args);
+		$title = apply_filters('widget_title', empty($instance['title']) ? __('W4 post list') : $instance['title'], $instance, $this->id_base);
+		echo $before_widget;
+		if( $title ) echo $before_title . $title . $after_title;
+		$this->_widget($instance);
+        echo $after_widget;
+	}
+	
+	function _widget($instance){
+		$category_ids = $instance['categories'] ;
+		
+		if(empty($category_ids) || !is_array($category_ids)){
+			echo "No category selected. Please select one to show its posts." ;
+			return ;
+		}
+
+		$category_ids = (array) $category_ids;
+		echo "<ul class=\"w4pl_parent\">";
+		foreach($category_ids as $category_id){
+			$category = get_category($category_id) ;
+			$category_name = $category->name ;
+			$category_title = "<a href=\"".get_category_link($category_id)."\" title=\"View $category_name\">$category_name &raquo;</a>";
+			
+			$category_posts_ids = get_objects_in_term($category->cat_ID, 'category');
+			$selected_posts_ids = array();
+			
+			foreach($category_posts_ids as $key => $category_posts_id){
+				if(get_post_meta($category_posts_id,'_w4pl_post'))
+					$selected_posts_ids[] = $category_posts_id;
+			}
+			
+			if($instance['show_category_posts_count'] == '1'){
+				$items = count($selected_posts_ids)>0 ? count($selected_posts_ids) : false ;
+			
+			}elseif($instance['show_category_posts_count'] == '2'){
+				$items = count($category_posts_ids)>0 ? count($category_posts_ids) : false ;
+			}
+	
+			$items = $items ? " <span class=\"item_count\">$items items</span>" : '' ;
+			
+			if($instance['show_category_posts_count'] != '0')
+				$category_title .= $items ;
+
+			echo "<li class=\"w4pl_list\">" ;
+			echo "<h3 class=\"w4pl_cat_title\">$category_title</h3>" ;
+			
+			if( $instance['show_post_list'] == '1' ):
+			$r = new WP_Query(array('cat' => $category->cat_ID, 'showposts' => '-1', 'posts_per_page' => '-1', 'meta_key' => '_w4pl_post', 'meta_value' => '1', 'post_status' => 'publish'));
+			//Checking post
+			if($r->have_posts()):
+				echo "<ul class=\"w4pl_sub\">" ;
+				while($r->have_posts()):
+					$r->the_post() ;
+					//$time = get_the_time( 'F jS, Y' ) ;
+					$time = get_the_time( 'j-m-Y' ) ;
+					$post_title = __("<a href=\"".get_permalink()."\" title=\"View ".get_the_title()."\">&raquo; ".get_the_title()."</a>") ;
+					if($instance['show_post_date'])
+						$post_title .= __(' <small> on ' . $time . '</small>') ;
+
+					echo "<li class=\"w4pl_post_list\">" ;
+					echo "<div class=\"w4pl_post_title\">$post_title</div>" ;
+
+					//Excerpt
+					if( $instance['post_content'] == '1' ){
+						$new_excerpt_length = create_function('$length', "return " . $instance["excerpt_length"] . ";");
+						add_filter('excerpt_length', $new_excerpt_length);
+					
+					
+						//add_filter( 'excerpt_length', array(&$this, 'excerpt_length'));
+						echo "<div class=\"w4pl_post_content\">".get_the_excerpt()."</div>" ;
+					}
+					
+					//Post content
+					if( $instance['post_content'] == '2' ){
+						echo "<div class=\"w4pl_post_content\">".get_the_content()."</div>" ;
+					}
+
+					echo "</li>" ;
+				endwhile; //End while
+				echo "</ul>" ;
+				wp_reset_postdata();
+			endif; //Show post
+			endif;
+			echo "</li>" ;
+		}
+		remove_filter( 'excerpt_length', $new_excerpt_length);
+		echo "</ul>" ;
+	}
+
+	function update( $new_instance, $old_instance ) {
+		$instance 								= $old_instance;
+		$instance['title'] 						= strip_tags( $new_instance['title']);
+		$instance['categories'] 				= (array)$new_instance['categories'];
+		$instance['show_category_posts_count']	= (int)$new_instance['show_category_posts_count'];
+		$instance['show_post_list'] 			= (bool) $new_instance['show_post_list'];
+		$instance['show_post_date'] 			= (bool) $new_instance['show_post_date'];
+		$instance['post_content']				= (int) $new_instance['post_content'];
+		$instance['excerpt_length']				= (int) $new_instance['excerpt_length'];
+		return $instance;
+	}
+
+
+	function form( $instance ){
+		$title 						= isset($instance['title']) ? esc_attr($instance['title']) : 'Hit list:';
+		$categories 				= isset($instance['categories']) ? (array)($instance['categories']) : array();
+		$show_category_posts_count 	= isset($instance['show_category_posts_count']) ? (int)($instance['show_category_posts_count']) : 1;
+		$show_post_list 			= isset($instance['show_post_list']) ? (bool)($instance['show_post_list']) : 1;
+		$show_post_date 			= isset($instance['show_post_date']) ? (bool)($instance['show_post_date']) : 1;
+		$post_content 				= isset($instance['post_content']) ? (int)($instance['post_content']) : 0;
+		$excerpt_length 			= isset($instance['excerpt_length']) ? (int)($instance['excerpt_length']) : 10;
+		
+		?>
+		<div id="w4pl_widget_admin">
+			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" 
+            value="<?php echo $title; ?>" /></p>
+            
+            <p>Select category:<br />
+			<?php echo $this->post_categories_checklist($this->get_field_name('categories'),$this->get_field_id('categories'),$categories); ?></p>
+            
+			<p><label for="<?php echo $this->get_field_id('show_category_posts_count'); ?>"><?php _e( 'Show category post count based on your selecton ?' ); ?></label>
+            <select name="<?php echo $this->get_field_name('show_category_posts_count'); ?>" id="<?php echo $this->get_field_id('show_category_posts_count'); ?>" class="widefat">
+            	<option value="0"<?php selected( $show_category_posts_count, '0' ); ?>><?php _e('Do not show'); ?></option>
+                <option value="1"<?php selected( $show_category_posts_count, '1' ); ?>><?php _e('Show only included post count'); ?></option>
+                <option value="2"<?php selected( $show_category_posts_count, '2' ); ?>><?php _e('Show the actual category count'); ?></option>
+            </select></p>
+            
+            <p><label for="<?php echo $this->get_field_id('show_post_list'); ?>"><?php _e( 'Show post list ?' ); ?></label>
+            <select name="<?php echo $this->get_field_name('show_post_list'); ?>" id="<?php echo $this->get_field_id('show_post_list'); ?>" class="widefat">
+            	<option value="0"<?php selected( $show_post_list, false ); ?>><?php _e('No'); ?></option>
+                <option value="1"<?php selected( $show_post_list, true ); ?>><?php _e('Yes'); ?></option>
+            </select></p>
+
+			<p><label for="<?php echo $this->get_field_id('show_post_date'); ?>"><?php _e( 'Show date appending post title ?' ); ?></label>
+            <select name="<?php echo $this->get_field_name('show_post_date'); ?>" id="<?php echo $this->get_field_id('show_post_date'); ?>" class="widefat">
+            	<option value="0"<?php selected( $show_post_date, false ); ?>><?php _e('No'); ?></option>
+                <option value="1"<?php selected( $show_post_date, true ); ?>><?php _e('Yes'); ?></option>
+            </select></p>
+            
+            <p><label for="<?php echo $this->get_field_id('post_content'); ?>"><?php _e( 'Show post content ?' ); ?></label>
+            <select name="<?php echo $this->get_field_name('post_content'); ?>" id="<?php echo $this->get_field_id('post_content'); ?>" class="widefat">
+            	<option value="0"<?php selected( $post_content, '0' ); ?>><?php _e('Do not show content'); ?></option>
+                <option value="1"<?php selected( $post_content, '1' ); ?>><?php _e('Show only excerpt'); ?></option>
+                <option value="2"<?php selected( $post_content, '2' ); ?>><?php _e('Show full content'); ?></option>
+            </select></p>
+
+            <p><label for="<?php echo $this->get_field_id('excerpt_length'); ?>"><?php _e('Excerpt length for posts/pages:'); ?></label>
+			<input type="text" value="<?php echo( $excerpt_length) ; ?>" name="<?php echo $this->get_field_name('excerpt_length'); ?>" 
+			id="<?php echo $this->get_field_id('excerpt_length'); ?>" class="widefat"/></p>
+		</div>
+		<?php
+	}
+	
+	function post_categories_checklist( $field_name = null, $field_id = null, $cat_array = array()){
+		$categories = get_categories(array('hide_empty' => false));
+		foreach( $categories as $category ){
+			if(in_array($category->cat_ID, $cat_array))
+				$checked = in_array($category->cat_ID, $cat_array) ? ' checked="checked" ' : '' ;
+			$checklists[] = "<label><input name=\"".$field_name."[]\" type=\"checkbox\" $checked value=\"$category->cat_ID\" /> $category->cat_name</label><br />" ;
+		}
+		$checklists = implode( "\n", $checklists );
+		return $checklists ;
+	}
+}
+//load Widget==============================
+add_action('widgets_init', 'W4PL_Widget_Init');
+function W4PL_Widget_Init() {
+  register_widget('W4PL_Widget');
+}
 //Begin=====================================
-	new W4PL();
+new W4PL_CORE();
 ?>
