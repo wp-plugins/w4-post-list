f8b8ea379e3d1530a9e40e52807db338e42c5a37
sajib1223 <sajib1223@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-22 20:25:11 +0000

    git-svn-id: https://plugins.svn.wordpress.org/w4-post-list/trunk@349401 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- w4-post-list.js
+++ w4-post-list.js
@@ -1,9 +1,9 @@
 (function($){
 function w4pl_admin_toogle(){
 	if ($(this).is(':checked')){
-		$(this).parent().parent().next().show();
+		$(this).parent().parent().parent().children("div.w4c_inside").show();
 	} else {
-		$(this).parent().parent().next().hide();
+		$(this).parent().parent().parent().children("div.w4c_inside").hide();
 	}
 }
 
@@ -19,8 +19,22 @@ function w4pl_toogle(){
 	}
 }
 
+$(function() {
+		$("ul.sortable").sortable({ opacity: 0.6, cursor: 'move'});
+	});
+
 $(document).ready(function($){
 	$(".w4pl_cat_checkbox").click(w4pl_admin_toogle);
 	$('span.showhide_w4pl').click(w4pl_toogle);
+	
+	$("#post_list_form .option").bind({
+		mouseover: function(){
+			$(".option input.save_list_option").remove();
+			$(this).append('<input type="submit" name="save_w4_post_list_options" class="save_list_option" value="Save option" />');
+		//},
+		//mouseout: function(){
+		//	$(this).children("input.save_list_option").remove();
+		}
+	});
 });
 })(jQuery) ;
