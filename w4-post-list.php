c948e530f1e3d612ec71a307bf5da45fd35721b4
sajib1223 <sajib1223@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-24 12:56:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/w4-post-list/trunk@350387 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- w4-post-list.php
+++ w4-post-list.php
@@ -7,6 +7,25 @@ Version: 1.3
 Author: Shazzad Hossain Khan
 Author URI: http://w4dev.com/
 */
+
+/*  Copyright 2011  Shazzad Hossain Khan  (email : sajib1223@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
 define( 'W4PL_DIR', plugin_dir_path(__FILE__)) ;
 define( 'W4PL_URL', plugin_dir_url(__FILE__)) ;
 define( 'W4PL_BASENAME', plugin_basename( __FILE__ )) ;
